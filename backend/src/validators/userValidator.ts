4c65754926c4828f5dcdb8d76b5564828b2876e3
Vini Black <viniibl4ck@gmail.com>
2025-05-30 19:49:20 -0300

    organizing repository

--- backend/src/validators/userValidator.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const registerUserSchema = z.object({
-  firstName: z.string().min(3),
-  lastName: z.string().min(3),
-  email: z.string().email(),
-  password: z.string().min(8),
-});
